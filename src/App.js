9aca7db8ad02e30b6eb9b3db6f690a5522a2bd5a
Tushar <vermatushar237@gmail.com>
2023-01-02 17:56:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <>
+ <div className="center">
+  <p><span className='hi'>Hey! I'm Tushar.</span>
+  <span></span> <br /><span className='profession'>Frontend developer, 16.</span>
+  <br />Currently exploring AI.
+  {/* Connect with me on <a href='https://twitter.com/tusharrvrma'>Twitter</a> */}
+  </p>
+        <div className="anime">
+
+ 
+               <p>
+                   <span className='first' >&nbsp;MY WORK &nbsp;X&nbsp; MY WORK &nbsp;X&nbsp; MY WORK &nbsp;X&nbsp; MY WORK &nbsp;X&nbsp;</span>
+                   <span className='second'>&nbsp;MY WORK &nbsp;X&nbsp; MY WORK &nbsp;X&nbsp; MY WORK &nbsp;X&nbsp; MY WORK &nbsp;X&nbsp;</span>
+
+                </p> 
+ 
+         </div>
+  <div className="build">
+    {/* <h2 className="Build-Heading">Builds</h2> */}
+     <a href='https://essay-generator-abc.onrender.com/' target={'_blank'}>✢ GPT-3 Essay Generator</a>&nbsp;<br />
+    generate ready-to-submit essays in just a few clicks.<br /><br />
+   <a href='https://SkyScan.tusharverma13.repl.co' target={'_blank'}>✢ SkyScan</a><br />
+get the latest weather conditions anywhere, anytime.<br /><br />
+   <a href='https://Split-It.tusharverma13.repl.co' target={'_blank'}>✢ Split-It</a><br />
+divide expenses fairly among friends and family.<br /><br />
+<a href='https://RockPaperScissors.tusharverma13.repl.co' target={'_blank'}>✢ RockPaperScissors</a><br />
+battle it out against AI.<br /><br />
+ <a href='https://Stopwatch.tusharverma13.repl.co' target={'_blank'}>✢ Stopwatch</a><br />
+time your activities with precision.
+
+  </div>
+  <hr />
+
+  <div className="socials">
+    <ul>
+      <li>    Twitter&nbsp;<a href="https://twitter.com/tusharrvrma" target={'_blank'} >@tusharrvrma</a>
+</li>
+      <li>    Instagram&nbsp;<a href='https://instagram.com/tushhhhaarr' target={'_blank'} >@tushhhhaarr</a>
+</li>
+      <li>    Github&nbsp;<a href='https://github.com/tusharverma06' target={'_blank'} >@tusharverma06</a>
+</li>
+      <li>    Email&nbsp;<a href='mailto:vermatushar237@gmailcom' target={'_blank'} >vermatushar237@gmail.com</a>
+</li>
+    </ul>
+  </div>
+ </div>
+ </>
   );
 }
 
